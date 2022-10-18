3dd5cc9f57fbe2961155caf48c0d1543074881e2
Чайковский Прохор <kemononya@yandex.ru>
2022-10-18 17:53:09 +0300

    make some part of second stage

--- src/init.js
+++ src/init.js
@@ -1,9 +1,13 @@
+import onChange from 'on-change';
+import render from './render.js';
+
 export default () => {
   const state = {};
+  const watchedState = onChange(state, render);
   const form = document.querySelector('form.rss-form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    console.log(formData.get('url'));
+    watchedState.url = formData.get('url');
   });
 };
